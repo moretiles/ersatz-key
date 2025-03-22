024678fa21bb03b60b8a8da630c9ab9f09b92a92
dvergano <verganodavid29@gmail.com>
2025-03-21 21:19:21 -0400

    Overhauling things
    Not in a finished state right now.

--- ersatz-key-shamir_installer.sh
+++ ersatz-key-shamir_installer.sh
@@ -2,10 +2,9 @@
 
 set -e
 
-# Setup 2fa the way I like it
+source ./scripts/config-file.sh
 
-cryptroot_name=usb-luks
-cryptroot_path=/dev/mapper/"${cryptroot_name}"
+# Setup 2fa the way I like it
 
 if [[ "$(id -u)" != "0" ]]; then
     printf "You are not root, this script requires root\n" 1>&2
@@ -14,19 +13,21 @@ if [[ "$(id -u)" != "0" ]]; then
 fi
 
 if [[ -z "${1}" ]]; then
-    printf "./install {LOCAL_SHAMIR_SEAL_PATH}" 1>&2
+    printf "./install {LOCAL_CONFIG_PATH}" 1>&2
     printf "It might be a good idea to look over the script before you install it\n" 1>&2
     exit 2
 fi
 
-local_shamir_seal_path="${1}"
+config_file="${1}"
+mapper_name="$(field 'mapper_name' "${config_file}")"
 
 chmod +x scripts/*
 mkdir -p /usr/local/bin/ersatz-key/keepass/
 cp scripts/shamir-keepass-decrypt.sh /usr/local/bin/ersatz-key/keepass
+cp scripts/config-file.sh /usr/local/bin/ersatz-key/config-file.sh
 cp units/keepassxc-shamir@.service /etc/systemd/system/
 
-systemctl enable keepassxc-shamir@"$(systemd-escape "${local_shamir_seal_path}")".service
+systemctl enable ersatz-key-shamir@"$(systemd-escape "${mapper_name}")".service
 #systemctl enable keepassxc-shamir@a.service
 
 systemctl daemon-reload
--- ersatz-key_installer.sh
+++ ersatz-key_installer.sh
@@ -4,8 +4,8 @@ set -e
 
 # Setup 2fa the way I like it
 
-cryptroot_name=usb-luks
-cryptroot_path=/dev/mapper/"${cryptroot_name}"
+mapper_name=usb-luks
+cryptroot_path=/dev/mapper/"${mapper_name}"
 
 if [[ "$(id -u)" != "0" ]]; then
     printf "You are not root, this script requires root\n" 1>&2
@@ -22,7 +22,7 @@ fi
 encrypted_partition="${1}"
 key_file="${2}"
 
-cryptsetup luksOpen --key-file="${key_file}" "${encrypted_partition}" "${cryptroot_name}"
+cryptsetup luksOpen --key-file="${key_file}" "${encrypted_partition}" "${mapper_name}"
 
 uuid_crypttab="$(blkid -o udev "${encrypted_partition}" | grep 'ID_FS_UUID=' | awk -F= '{print $2}')"
 uuid_fstab="$(blkid -o udev "${cryptroot_path}" | grep 'ID_FS_UUID=' | awk -F= '{print $2}')"
@@ -33,14 +33,14 @@ printf "\n" >> /etc/fstab
 echo "# ${cryptroot_path}" >> /etc/fstab
 printf "UUID=%s\t%s\t%s\t%s\t%s\t%s\n" "${uuid_fstab}" /usb ext4 rw,relatime 0 2 >> /etc/fstab
 
-cryptsetup luksClose "${cryptroot_name}"
+cryptsetup luksClose "${mapper_name}"
 
 chmod +x scripts/*
 mkdir -p /usr/local/bin/ersatz-key
 cp scripts/unmount-and-seal.sh /usr/local/bin/ersatz-key
-cp units/unmount-usb.service units/unseal-usb.target /etc/systemd/system
+cp units/ersatz-key-unseal@.target units/ersatz-key-unmount.service /etc/systemd/system
 
-systemctl enable unmount-usb.service
-systemctl enable unseal-usb.target
+systemctl enable ersatz-key-unseal@"$(systemd-escape "${mapper_name}")".target
+systemctl enable ersatz-key-unmount@"$(systemd-escape "${mapper_name}")".service
 
 systemctl daemon-reload
